dba71c86649069f9df49ba03e93bc7843e9d1502
Jonas A. Hultén <hulten@technocreatives.com>
2022-05-09 13:53:21 +0200

    Added tiny skeleton of Albatross

---
+++ albatross.py
@@ -0,0 +1,20 @@
+# albatross.py
+
+"""
+Albatross - A tool for migrating a GitLab.com group/project to a self-hosted instance
+
+Copyright (c) 2022 THETC The Techno Creatives AB
+"""
+
+import click
+import typing
+import gitlab
+
+
+def main() -> None:
+    pass
+
+
+# For invocation from the commandline
+if __name__ == "__main__":
+    main()
